ae977c0a30b4cf5011a04d07b97ef198a2cddb04
Pandyo <167075494+Pandyo@users.noreply.github.com>
2025-10-07 17:16:16 +0900

    Update s3.tf

--- scenarios/test/s3.tf
+++ scenarios/test/s3.tf
@@ -5,7 +5,7 @@ resource "aws_s3_bucket" "s3" {
 
 #s3 버킷 버전 관리
 resource "aws_s3_bucket_versioning" "s3_versioning" {
-  bucket = aws_s3_bucket.codebuild_s3.id #s3 지정
+  bucket = aws_s3_bucket.s3.id #s3 지정
   versioning_configuration {
     status = "Disabled" #버전 관리 비활성화
   }
@@ -13,7 +13,7 @@ resource "aws_s3_bucket_versioning" "s3_versioning" {
 
 #s3 버킷 기본 암호화
 resource "aws_s3_bucket_server_side_encryption_configuration" "s3_encryption" {
-  bucket = aws_s3_bucket.codebuild_s3.id #s3 지정
+  bucket = aws_s3_bucket.s3.id #s3 지정
 
   rule {
     apply_server_side_encryption_by_default {
@@ -24,7 +24,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "s3_encryption" {
 
 #s3 버킷 퍼블릭 액세스 차단 설정
 resource "aws_s3_bucket_public_access_block" "s3_public_access" {
-  bucket = aws_s3_bucket.codebuild_s3.id #s3 지정
+  bucket = aws_s3_bucket.s3.id #s3 지정
 
   block_public_acls       = true #모든 퍼블릭 액세스 차단
   block_public_policy     = true
