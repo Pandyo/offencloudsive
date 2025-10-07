77b8472c7a87a1e66f5d39ac990122def6ddbe23
Pandyo <167075494+Pandyo@users.noreply.github.com>
2025-10-07 17:07:20 +0900

    Update backend.tf

--- scenarios/test/backend.tf
+++ scenarios/test/backend.tf
@@ -7,12 +7,4 @@ terraform {
       version = ">= 4.67.0"
     }
   }
-
-  backend "s3" {
-    bucket         = "Bucket-Name"
-    key            = "terraform/terraform.tfstate"
-    region         = "ap-northeast-2"
-    encrypt        = true
-    dynamodb_table = "terraform-lock"
-  }
 }
